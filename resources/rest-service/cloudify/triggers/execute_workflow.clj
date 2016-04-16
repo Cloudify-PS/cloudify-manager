a8f61f2b9415654e145922ae7031a8aacb80d320
Noa Kuperberg <noakup@gmail.com>
2016-04-16 18:59:52 +0300

    updated clj settings

--- resources/rest-service/cloudify/triggers/execute_workflow.clj
+++ resources/rest-service/cloudify/triggers/execute_workflow.clj
@@ -2,10 +2,13 @@
   [ctx]
     (let [deployment-id         (:deployment_id ctx)
           parameters            (:trigger-parameters ctx)
-          manager-ip            (or (System/getenv "MANAGER_REST_HOST") "127.0.0.1")
+          manager-rest-host     (or (System/getenv "MANAGER_REST_HOST") "127.0.0.1")
+          manager-rest-protocol (or (System/getenv "MANAGER_REST_PROTOCOL") "http")
           raw-manager-rest-port (or (System/getenv "MANAGER_REST_PORT") "80")
           manager-rest-port     (Integer/parseInt raw-manager-rest-port)
-          base-uri              (str "http://" manager-ip ":" manager-rest-port "/api/v2")
+          raw-verify-ssl-cert   (or (System/getenv "VERIFY_SSL_CERTIFICATE") "false")
+          verify-ssl-cert       (Boolean/valueOf raw-verify-ssl-cert)
+          base-uri              (str manager-rest-protocol "://" manager-rest-host ":" manager-rest-port "/api/v2")
           endpoint              (str "/executions")
           resource-uri          (str base-uri endpoint)
           body                  (cheshire.core/generate-string {
@@ -19,4 +22,5 @@
          :accept         :json
          :socket-timeout (:socket_timeout parameters)
          :conn-timeout   (:conn_timeout parameters)
+         :insecure?      (not= verify-ssl-cert true)
          :body           body})))
