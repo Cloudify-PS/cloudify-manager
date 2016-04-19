9af39ac29cc12764866a9f61a541d10dd33fb853
gilzellner <gil.zellner@gmail.com>
2016-04-19 11:47:40 +0300

    CFY-5210-move-restservice-packaging-to-omnibus

--- packaging/restservice/omnibus/config/projects/rest-service.rb
+++ packaging/restservice/omnibus/config/projects/rest-service.rb
@@ -12,15 +12,14 @@ override :cacerts, version: '2015.10.28', source: { md5: '06629db7f712ff3a75630e
 override :pip, version: '7.1.2', source: { md5: '3823d2343d9f3aaab21cf9c917710196' }
 override :setuptools, version: '18.5', source: { md5: '533c868f01169a3085177dffe5e768bb' }
 
-# Defaults to C:/cloudify on Windows
-# and /opt/cloudify on all other platforms
+# Defaults to /opt/cloudify
 install_dir "#{default_root}/#{name}"
 
 build_version Omnibus::BuildVersion.semver
 build_iteration 1
 
 # Creates required build directories
-dependency "preparation"
+# dependency "preparation"
 
 # cloudify dependencies/components
 dependency "python"
