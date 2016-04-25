6119704887ce996717a6c7db0810f9cba1e28125
gilzellner <gil.zellner@gmail.com>
2016-04-25 18:40:40 +0300

    CFY-5210-move-restservice-packaging-to-omnibus

--- packaging/restservice/provision.sh
+++ packaging/restservice/provision.sh
@@ -21,12 +21,25 @@
 
 # VERSION/PRERELEASE/BUILD are exported to follow with our standard of exposing them as env vars. They are not used.
 CORE_TAG_NAME="3.4m4"
-curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/$CORE_TAG_NAME/common/provision.sh -o ./common-provision.sh &&
+curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/${PACKAGER_BRANCH-$CORE_TAG_NAME}/common/provision.sh -o ./common-provision.sh &&
 source common-provision.sh
 
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
+MANAGER_BRANCH=$3
+PACKAGER_BRANCH=$4
 
-#build_rpm &&
-#cd /tmp/x86_64 && create_md5 "rpm" &&
-#[ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "rpm" && upload_to_s3 "md5"
+rm -rf cloudify-manager
+git clone https://github.com/cloudify-cosmo/cloudify-manager.git
+cd cloudify-manager/packaging/restservice/omnibus
+git checkout ${MANAGER_BRANCH-$CORE_TAG_NAME}
+git tag -d $CORE_TAG_NAME
+NEW_TAG_NAME="${VERSION}.${PRERELEASE}"
+git tag $NEW_TAG_NAME
+omnibus build cloudify && result="success"
+cd pkg
+cat *.json || exit 1
+rm -f version-manifest.json
+
+[ "$result" == "success" ] && create_md5 "rpm" &&
+[ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "rpm" && upload_to_s3 "md5"
